f75efe9a9dfd29789564fc649e05a7edc86cc8d8
beyk <beykmail@gmail.com>
2022-03-11 18:47:06 +0330

    Modify some function description

--- src/index.test.js
+++ src/index.test.js
@@ -15,31 +15,31 @@ const removeIcon = (newId, todo) => {
 };
 
 describe('testing the addTodo function', () => {
-  test('check if items have been added to the list', () => {
+  test('check if string has been added to the list', () => {
     addTodo(todo, 'string');
 
     expect(todo.length).toBe(1);
   });
-  test('check if items have been added to the list', () => {
+  test('check if value has been added to the list', () => {
     addTodo(todo, 'value');
 
     expect(todo[1].description).toEqual('value');
   });
-  test('check if items have been added to the list', () => {
+  test('check if hello has been added to the list', () => {
     addTodo(todo, 'hello');
 
     expect(todo[2].description).toEqual('hello');
   });
 });
 
-describe('remove the removeIcon function', () => {
-  test('check if the selected items have been removed', () => {
+describe('check the remove function', () => {
+  test('check if the first selected item has been removed', () => {
     removeIcon(2, todo);
 
     expect(todo[0].index).toEqual(1);
   });
 
-  test('check if the selected items have been removed', () => {
+  test('check if the second selected item has been removed', () => {
     removeIcon(1, todo);
 
     expect(todo[2].description).toEqual('hello');
